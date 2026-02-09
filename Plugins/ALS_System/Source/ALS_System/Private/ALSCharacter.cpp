a516bc2f26af6d6c7855a6eec56b663a9029445f
Jihaoran <jihaoran@of3d.com>
2026-02-09 18:26:38 +0800

    Add: Add ALSCharacter

--- Plugins/ALS_System/Source/ALS_System/Private/ALSCharacter.cpp
+++ Plugins/ALS_System/Source/ALS_System/Private/ALSCharacter.cpp
@@ -3,3 +3,101 @@
 
 #include "ALSCharacter.h"
 
+AALSCharacter::AALSCharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	HeldObjectRoot = CreateDefaultSubobject<USceneComponent>(TEXT("HeldObjectRoot"));
+	HeldObjectRoot->SetupAttachment(GetMesh());
+
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	SkeletalMesh->SetupAttachment(HeldObjectRoot);
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(HeldObjectRoot);
+}
+
+void AALSCharacter::ClearHeldObject()
+{
+	StaticMesh->SetStaticMesh(nullptr);
+	SkeletalMesh->SetSkeletalMesh(nullptr);
+	SkeletalMesh->SetAnimInstanceClass(nullptr);
+}
+
+void AALSCharacter::AttachToHand(UStaticMesh* NewStaticMesh, USkeletalMesh* NewSkeletalMesh, class UClass* NewAnimClass, bool bLeftHand, FVector Offset)
+{
+	ClearHeldObject();
+
+	if (IsValid(NewStaticMesh))
+	{
+		StaticMesh->SetStaticMesh(NewStaticMesh);
+	}
+	else if (IsValid(NewSkeletalMesh))
+	{
+		SkeletalMesh->SetSkeletalMesh(NewSkeletalMesh);
+		if (IsValid(NewAnimClass))
+		{
+			SkeletalMesh->SetAnimInstanceClass(NewAnimClass);
+		}
+	}
+
+	FName AttachBone;
+	if (bLeftHand)
+	{
+		AttachBone = TEXT("VB LHS_ik_hand_gun");
+	}
+	else
+	{
+		AttachBone = TEXT("VB RHS_ik_hand-gun");
+	}
+
+	HeldObjectRoot->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, AttachBone);
+	HeldObjectRoot->SetRelativeLocation(Offset);
+}
+
+void AALSCharacter::RagdollStart()
+{
+	ClearHeldObject();
+	Super::RagdollStart();
+}
+
+void AALSCharacter::RagdollEnd()
+{
+	Super::RagdollEnd();
+	UpdateHeldObject();
+}
+
+ECollisionChannel AALSCharacter::GetThirdPersonTraceParams(FVector& TraceOrigin, float& TraceRadius)
+{
+	const FName CameraSocketName = bRightShoulder ? TEXT("TP_CameraTrace_R") : TEXT("TP_CameraTrace-L");
+	TraceOrigin = GetMesh()->GetSocketLocation(CameraSocketName);
+	TraceRadius = 15.0f;
+	return ECC_Camera;
+}
+
+FVector AALSCharacter::GetFirstPersonCameraTarget()
+{
+	return GetMesh()->GetSocketLocation(TEXT("FP_Camera"));
+}
+
+FTransform AALSCharacter::GetThirdPersonPivotTarget()
+{
+	return FTransform(GetActorRotation(),
+					  (GetMesh()->GetSocketLocation(TEXT("Head")) + GetMesh()->GetSocketLocation(TEXT("root"))) / 2.0f,
+					  FVector::OneVector);
+}
+
+void AALSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+}
+
+void AALSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AALSCharacter::OnOverlayStateChanged(EALSOverlayState PreviousState)
+{
+	Super::OnOverlayStateChanged(PreviousState);
+	UpdateHeldObject();
+}
--- Plugins/ALS_System/Source/ALS_System/Public/ALSCharacter.h
+++ Plugins/ALS_System/Source/ALS_System/Public/ALSCharacter.h
@@ -9,9 +9,46 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable, BlueprintType)
 class ALS_SYSTEM_API AALSCharacter : public AALSBaseCharacter
 {
 	GENERATED_BODY()
-	
+public:
+	AALSCharacter(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "ALS|Character")
+	void UpdateHeldObject();
+
+	UFUNCTION(BlueprintCallable, Category = "ALS|HeldObject")
+	void ClearHeldObject();
+
+	UFUNCTION(BlueprintCallable, Category = "ALS|HeldObject")
+	void AttachToHand(UStaticMesh* NewStaticMesh, USkeletalMesh* NewSkeletalMesh, class UClass* NewAnimClass, bool bLeftHand, FVector Offset);
+
+	virtual void RagdollStart() override;
+
+	virtual void RagdollEnd() override;
+
+	virtual ECollisionChannel GetThirdPersonTraceParams(FVector& TraceOrigin, float& TraceRadius) override;
+
+	virtual FVector GetFirstPersonCameraTarget() override;
+
+	virtual FTransform GetThirdPersonPivotTarget() override;
+protected:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+
+	virtual void OnOverlayStateChanged(EALSOverlayState PreviousState) override;
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS|Component")
+	TObjectPtr<USceneComponent> HeldObjectRoot = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS|Component")
+	TObjectPtr<USkeletalMeshComponent> SkeletalMesh = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS|Component")
+	TObjectPtr<UStaticMeshComponent> StaticMesh = nullptr;
+private:
+	bool bNeedsColorReset = false;
 };
